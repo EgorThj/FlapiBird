fd4646d5f12d17c775932edf13de3e594f7d3da2
EgorThj <eegortelnoy@gmail.com>
2026-08-09 15:00:09 +0300

    much

--- main.js
+++ main.js
@@ -10,13 +10,35 @@ kaplay({
 });
 loadSprite("bird", "Flapybird.png")
 loadSprite("bg", "Background.png")
+setGravity(180)
 let bird = add([
     sprite("bird"),
+    pos(20,150),
     scale(0.07),
+    body(),
+    area(),
+    "bird"
 ]);
+let groud =add([
+    rect(400,10),
+    body({
+        isStatic:true
+    }),
+    area(),
+    pos(0,200),
+    color(0,43,0),
+    "enemy"
+
+])
 let bg = add([
     sprite("bg",{width:width(),height:height()}),
     pos(0,0),
     fixed(), 
     z(-100)
 ]);
+onKeyPress("space",function(){
+    bird.jump(100)
+})
+onCollide("enemy","bird",function(){
+    console.log("wewrewer")
+})
